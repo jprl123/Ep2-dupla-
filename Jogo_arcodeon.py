6a48c4ec8506c5b9041438a9ff44ff30c3be4686
jprl123 <joaoprl1@al.insper.edu.br>
2021-05-10 22:18:20 -0300

    implementando os limites de movimento

--- Jogo_arcodeon.py
+++ Jogo_arcodeon.py
@@ -1,2 +1,28 @@
-import cria_baralho 
-print(cria_baralho)
+import cria_baralho
+import Extrai_naipe
+import empilha_carta
+import extrair_valor
+import Possui_movimentos
+import movimenta_listas 
+
+baralho = cria_baralho() #criando baralho
+
+pm = Possui_movimentos(baralho)
+
+while pm:
+    pergunta1 = int(input('Qual carta voce quer ?(digite um numero de 1 - 52'))
+    if pergunta1 <= 0 or pergunta1 > 52 or pergunta1 == 1:
+        print('movimento invalido')
+        return pergunta1
+    
+
+
+    
+    
+
+
+
+
+
+
+
