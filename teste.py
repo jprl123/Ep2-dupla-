149d2b864e2d28386f4997ace9c10a79534a94c3
jprl123 <81189398+jprl123@users.noreply.github.com>
2021-05-10 21:15:56 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,26 +1,3 @@
-'''def cria_baralho():
-    valores = ['A',2,3,4,5,6,7,8,9,10,'J','Q','K']
-    lista = []
-    for i in valores:
-        baralho = [(valores)'♠', '(valores[i])♥','(valores[i])♣', '(valores[i])♦']
-        lista.append([baralho])
-    return lista
 
-print(cria_baralho())'''
-
-
-def cria_baralho():
-    baralho = []
-    valores = ['A',2,3,4,5,6,7,8,9,10,'J','Q','K','A',2,3,4,5,6,7,8,9,10,'J','Q','K','A',2,3,4,5,6,7,8,9,10,'J','Q','K','A',2,3,4,5,6,7,8,9,10,'J','Q','K']
-    naipe=['♠', '♥', '♦', '♣','♠', '♥', '♦', '♣','♠', '♥', '♦', '♣','♠', '♥', '♦', '♣','♠', '♥', '♦', '♣','♠', '♥', '♦', '♣','♠', '♥', '♦', '♣','♠', '♥', '♦', '♣']
-    i = 0
-    juntar = 0
-    while i < len(naipe):
-        juntar = '{0} {1}'.format(valores[i], naipe[i])
-        baralho.append(juntar)
-        i +=1
-    return baralho
-
-print(cria_baralho())
 
     
