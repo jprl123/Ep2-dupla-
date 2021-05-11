ce1ba654fe11cbeae3de5260ef9e60eceb9d6d5b
lucasgurgeln <lucasg3@al.insper.edu.br>
2021-05-11 09:59:00 -0300

    Jogo implementação e algumas correções

--- Jogo_arcodeon.py
+++ Jogo_arcodeon.py
@@ -1,19 +1,32 @@
-import cria_baralho
-import Extrai_naipe
-import empilha_carta
-import extrair_valor
-import Possui_movimentos
-import movimenta_listas 
-
-baralho = cria_baralho() #criando baralho
-
-pm = Possui_movimentos(baralho)
-
+from cria_baralho import cria_baralho
+from Extrai_naipe import extrai_naipe
+from empilha_carta import empilha
+from extrair_valor import extrai_valor
+from Possui_movimentos import possui_movimentos_possiveis
+from movimenta_listas import lista_movimentos_possiveis 
+baralhon = cria_baralho() #criando baralho
+pm = possui_movimentos_possiveis(baralhon)
+pm=True
 while pm:
-    pergunta1 = int(input('Qual carta voce quer ?(digite um numero de 1 - 52'))
-    if pergunta1 <= 0 or pergunta1 > 52 or pergunta1 == 1:
+    pergunta1 = int(input('Qual carta voce quer ?(digite um numero de 1 - 52)  '))
+    carta=baralhon[pergunta1 - 1]
+    ali= int(pergunta1)
+    if pergunta1 <= 1 or pergunta1 > 52:
         print('movimento invalido')
-        return pergunta1
+    lmp=lista_movimentos_possiveis(baralhon, ali)
+    if lmp == [1, 3]:
+        pergunta2=input('Você quer empilhar 1 ou 3?')
+        if pergunta2 == '1':
+            empilha(baralhon, baralhon[pergunta1], baralhon[pergunta1-1])
+        elif pergunta2 == '3':
+            empilha(baralhon, baralhon[pergunta1], baralhon[pergunta1-3])
+    elif lmp == [1]:
+        empilha(baralhon, baralhon[pergunta1], baralhon[pergunta1-1])
+    elif lmp == [3]:
+        empilha(baralhon, baralhon[pergunta1], baralhon[pergunta1-3])
+
+
+
     
 
 
