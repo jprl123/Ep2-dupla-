724cdc6f664b1c4d5332829b1e5b71f646928e0b
jprl123 <joaoprl1@al.insper.edu.br>
2021-05-10 21:15:11 -0300

    6º exercicio

---
+++ Possui_movimentos.py
@@ -0,0 +1,24 @@
+def extrai_valor(string):
+    valor=string[0:len(string)-1]
+    return valor
+def extrai_naipe(string):
+    naipe=string[-1]
+    return naipe    
+def possui_movimentos_possiveis(lista):
+    x = 0
+    for po in lista:
+        vc3=extrai_valor(lista[x-3])
+        nc3=extrai_naipe(lista[x-3])
+        vca=extrai_valor(lista[x-1])
+        nca=extrai_naipe(lista[x-1])
+        vc=extrai_valor(lista[x])
+        nc=extrai_naipe(lista[x])
+        if vca==vc or nca==nc:
+            return True
+        if vc3==vc or nc3==nc:
+            return True
+        x+=1   
+    else:
+        return False
+ex = ['A♦', '10♥', 'Q♣', 'K♠', '10♣', '4♠']
+print(possui_movimentos_possiveis(ex))
