5547627f684e9443de65f96b9549391ef72da028
jprl123 <joaoprl1@al.insper.edu.br>
2021-05-11 21:49:31 -0300

    Jogo funcionando só falta as cores

--- Jogo_arcodeon.py
+++ Jogo_arcodeon.py
@@ -4,29 +4,59 @@ from empilha_carta import empilha
 from extrair_valor import extrai_valor
 from Possui_movimentos import possui_movimentos_possiveis
 from movimenta_listas import lista_movimentos_possiveis 
+RED   = "\033[1;31m"  
+BLUE  = "\033[1;34m"
+CYAN  = "\033[1;36m"
+GREEN = "\033[0;32m"
+RESET = "\033[0;0m"
+BOLD    = "\033[;1m"
+REVERSE = "\033[;7m"
+
 
 
 
 
 print('-------------------------------------------------------------------------------------')
-baralhon = cria_baralho() #criando baralho
+print('Seja bem-vindo(a) ao jogo de Paciência Acordeão! O objetivo deste jogo é colocar todas as cartas em uma mesma pilha.') 
+print('')
+print('Existem apenas dois movimentos possíveis:') 
+print('')
+print('1. Empilhar uma carta sobre a carta imediatamente anterior;') 
+print('2. Empilhar uma carta sobre a terceira carta anterior.') 
+print('')
+print('Para que um movimento possa ser realizado basta que uma das duas condições abaixo seja atendida: ')
+print('')
+print('1. As duas cartas possuem o mesmo valor ou') 
+print('2. As duas cartas possuem o mesmo naipe.' )
+print('')
+print('Desde que alguma das condições acima seja satisfeita, qualquer carta pode ser movimentada.') 
+print('')
+print(input('Aperte [Enter] para iniciar o jogo...'))
+print('-------------------------------------------------------------------------------------')
+print('')
+
+
+
+baralhon = cria_baralho() 
 pm = possui_movimentos_possiveis(baralhon)
 pm=True
+
 while pm:
     for i in baralhon:
         for c in range(1,len(baralhon)+1):
             print (c,baralhon[c-1])
         pergunta1 = int(input('Qual carta voce quer?(digite um numero de 1 - {})  '.format(len(baralhon))))
         if pergunta1 <= 1 or pergunta1 > len(baralhon):
-            print('movimento invalido')
+            print('Movimento Invalido')
+            continue
         else:
             print('A carta selecionada é {}'.format(baralhon[pergunta1-1]))
             lmp=lista_movimentos_possiveis(baralhon, pergunta1-1)
             if lmp == [1, 3]:
                 print('Você quer empilhar a carta {} em qual posicao? '.format(baralhon[pergunta1-1]))
-                print('1. {}'.format(baralhon[pergunta1-2]))
-                print('2. {}'.format(baralhon[pergunta1-4]))
-                pergunta2 =input('Qual opção voce deseja vossa excelencia?')
+                print(RED+('1. {}'.format(baralhon[pergunta1-2])+RESET))
+                print(BLUE+('2. {}'.format(baralhon[pergunta1-4])+RESET))
+                pergunta2 =input('Qual opção voce deseja? ')
                 if pergunta2 != 1 or pergunta2 !=2:
                     print('opção invalida')
                 if pergunta2 == '1':
@@ -39,22 +69,21 @@ while pm:
                     empilha(baralhon,pergunta1-1, pergunta1-4)
             else:
                 print('A carta selecionada {} não pode ser movida, escolha novamente'.format(baralhon[pergunta1-1]))
-            pm = possui_movimentos_possiveis(baralhon)
-    if pm ==False:
-        if c==0:
-            gn=input('Você jogou de maneira elegante! Quer jogar novamente? (sim ou nao) ')
-            if gn=='nao':
-                print('Acabou')
-                pm=False
-            elif gn == 'sim':
-                pm=True
-        else:
-            jn=input('Você jogou de maneira deselegante! Quer tentar novamente? (sim ou nao)   ')
-            if jn=='nao':
-                print('Acabou')
-                pm=False
-            elif jn == 'sim':
-                pm=True
+    if len(baralhon) == 1:
+        print('Você jogou de maneira elegante! Parabéns')
+    else:
+        print('Que pena, você jogou de maneira deselegante!')
+    gn=input('Quer jogar novamente? (sim ou nao) ')
+    if gn == 'sim':
+        print('-----------------------------------------------------------------------------------------')
+        baralhon = cria_baralho() 
+        continue
+    elif gn == 'nao':
+        break
+print('Obrigado por jogar!!!')
+    
+    
+    
 
 
 
--- cria_baralho.py
+++ cria_baralho.py
@@ -4,14 +4,11 @@ def cria_baralho():
     cartas=['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K']
     baralho=[]
     for i in cartas:
-        baralho.append('{}♠'.format(i))
-        baralho.append('{}♥'.format(i))
-        baralho.append('{}♦'.format(i))
-        baralho.append('{}♣'.format(i))
+        baralho.append('- {}♠'.format(i))
+        baralho.append('- {}♥'.format(i))
+        baralho.append('- {}♦'.format(i))
+        baralho.append('- {}♣'.format(i))
     random.shuffle(baralho)
     return baralho
-
-
-
 
    
